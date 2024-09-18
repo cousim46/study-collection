f67db7f73ae8fc6aed5470c4337884ee8e7eb330
cousim46 <wjdghldns24@naver.com>
2024-09-18 22:08:43 +0900

    config : rabbitMQ 설정

--- rabbitMQ-receiver/receiver/src/main/java/rabbitmq/receiver/ReceiverApplication.java
+++ rabbitMQ-receiver/receiver/src/main/java/rabbitmq/receiver/ReceiverApplication.java
@@ -1,9 +1,11 @@
 package rabbitmq.receiver;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class ReceiverApplication {
 
     public static void main(String[] args) {
