f8900827a500dced6c7ad9917bd29323cb09ec2c
cousim46 <wjdghldns24@naver.com>
2024-11-03 19:14:11 +0900

    create project

---
+++ transaction-propagation/propagation/src/main/java/transaction/propagation/PropagationApplication.java
@@ -0,0 +1,13 @@
+package transaction.propagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropagationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropagationApplication.class, args);
+    }
+
+}
