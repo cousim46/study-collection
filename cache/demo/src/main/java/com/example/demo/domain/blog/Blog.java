ed7d6b8d7ec737135c5919936acf3d5d568445f8
cousim46 <wjdghldns24@naver.com>
2025-03-23 00:12:33 +0900

    feat : blog 연관관계 설정

--- cache/demo/src/main/java/com/example/demo/domain/blog/Blog.java
+++ cache/demo/src/main/java/com/example/demo/domain/blog/Blog.java
@@ -1,9 +1,12 @@
 package com.example.demo.domain.blog;
 
+import com.example.demo.domain.account.Account;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,4 +22,7 @@ public class Blog {
     private String name;
     private String url;
     private int totalHit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account account;
 }
