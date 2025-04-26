b90b14c4e3e0bbdd4b6b0d0a3446abf72268d2e6
cousim46 <wjdghldns24@naver.com>
2025-04-27 02:56:24 +0900

    test : 자바 ReentrantLock 사용하여 스핀락 테스트코드 작성

---
+++ spin-distrubuted-lock/lock/src/test/java/study/lock/java/spin/JavaSpinLockTest.java
@@ -0,0 +1,28 @@
+package study.lock.java.spin;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class JavaSpinLockTest {
+
+    @Test
+    void executeJavaSpinLock() throws InterruptedException {
+        int threadCount = 5;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        Counter counter = new Counter();
+        CountDownLatch doneSignal = new CountDownLatch(threadCount);
+        JavaSpinLock spinLock = new JavaSpinLock();
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(() -> {
+                spinLock.executeSpinLock(counter);
+                doneSignal.countDown();
+            });
+        }
+        doneSignal.await();
+        executorService.shutdown();
+        Assertions.assertThat(counter.getCount()).isEqualTo(threadCount);
+    }
+}
