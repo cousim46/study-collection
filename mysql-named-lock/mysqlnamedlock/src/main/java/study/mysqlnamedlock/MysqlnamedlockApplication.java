5ec092384ca84b2dbf6c26f667b4d04c3878d372
cousim46 <wjdghldns24@naver.com>
2025-05-13 20:57:03 +0900

    MySQL 네임드락 학습을 위한 프로젝트 세팅

---
+++ mysql-named-lock/mysqlnamedlock/src/main/java/study/mysqlnamedlock/MysqlnamedlockApplication.java
@@ -0,0 +1,13 @@
+package study.mysqlnamedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlnamedlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlnamedlockApplication.class, args);
+    }
+
+}
---
+++ mysql-named-lock/mysqlnamedlock/src/test/java/study/mysqlnamedlock/MysqlnamedlockApplicationTests.java
@@ -0,0 +1,13 @@
+package study.mysqlnamedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlnamedlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
