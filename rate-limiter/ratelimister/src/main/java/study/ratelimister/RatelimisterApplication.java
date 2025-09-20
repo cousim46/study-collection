98cee7703877f590eeb60d4fb7dcbf7934771a67
cousim46 <wjdghldns24@naver.com>
2025-09-20 23:27:17 +0900

    rate-limiter study

---
+++ rate-limiter/ratelimister/src/main/java/study/ratelimister/RatelimisterApplication.java
@@ -0,0 +1,13 @@
+package study.ratelimister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelimisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatelimisterApplication.class, args);
+    }
+
+}
---
+++ rate-limiter/ratelimister/src/test/java/study/ratelimister/RatelimisterApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ratelimister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
