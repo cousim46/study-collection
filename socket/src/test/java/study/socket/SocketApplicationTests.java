ca838e2730a68df02f41f66abdec1d9e0ee7e398
cousim46 <wjdghldns24@naver.com>
2025-11-08 19:49:21 +0900

    socket 학습 프로젝트 생성

---
+++ socket/src/test/java/study/socket/SocketApplicationTests.java
@@ -0,0 +1,13 @@
+package study.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
